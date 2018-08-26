2abb40cd870a02e4a3f14c0c331efdb8d1ba840e
Simon Liu <cyamonide@gmail.com>
2018-08-26 03:36:57 +0000

    Create basic landing page

--- landing/urls.py
+++ landing/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('resume/', views.resume, name='resume'),
+    path('', views.landing, name='landing'),
+    path('resume/', views.resume, name='resume'),
 ]
--- landing/views.py
+++ landing/views.py
@@ -3,6 +3,9 @@ from django.http import FileResponse, Http404
 
 # Create your views here.
 
+def landing(request):
+  return render(request, 'landing/landing.html', {})
+
 def resume(request):
   try:
     return FileResponse(open('/home/cyamo/mysite/static/documents/resume.pdf', 'rb'), content_type='application/pdf')
