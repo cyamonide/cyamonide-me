e5d1d0efdf1a0e4f94d1a4b278b034e1815b1ec9
Simon Liu <cyamonide@gmail.com>
2019-04-05 11:03:31 -0400

    Clean directory

--- landing/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.landing, name='landing'),
-    path('resume/', views.resume, name='resume'),
-    path('what-i-use/', views.view_site_tools, name='what-i-use'),
-]
--- notes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('', views.notes_list, name='notes-list'),
-  path('view_note/<str:course>/<str:filename>/', views.notes_detail),
-]
