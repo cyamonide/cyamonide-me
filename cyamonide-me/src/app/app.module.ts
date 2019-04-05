4e89f90f95342b6de96155da28216d985df7f701
Simon Liu <cyamonide@gmail.com>
2019-04-05 11:40:09 -0400

    Generate github module components

--- cyamonide-me/src/app/app.module.ts
+++ cyamonide-me/src/app/app.module.ts
@@ -3,22 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreComponent } from './core/core.component';
-import { SharedComponent } from './shared/shared.component';
-import { LandingComponent } from './landing/landing.component';
-import { GithubComponent } from './github/github.component';
-import { ProfileComponent } from './profile/profile.component';
-import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CoreComponent,
-    SharedComponent,
-    LandingComponent,
-    GithubComponent,
-    ProfileComponent,
-    ResumeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- cyamonide-me/src/app/github/github.module.ts
+++ cyamonide-me/src/app/github/github.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from './card/card.component';
+import { GithubComponent } from './github.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CardComponent, GithubComponent],
   imports: [
     CommonModule
   ]
