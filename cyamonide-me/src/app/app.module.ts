5a91d59d8fd7b5021adfcf5540f6cf1e0c4a665b
Simon Liu <cyamonide@gmail.com>
2019-04-05 11:27:54 -0400

    Generate page components

--- cyamonide-me/src/app/app.module.ts
+++ cyamonide-me/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreComponent } from './core/core.component';
 import { SharedComponent } from './shared/shared.component';
+import { LandingComponent } from './landing/landing.component';
+import { GithubComponent } from './github/github.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoreComponent,
-    SharedComponent
+    SharedComponent,
+    LandingComponent,
+    GithubComponent,
+    ProfileComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
