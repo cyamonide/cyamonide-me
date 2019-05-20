ff33d3e1e63c674d91ff5f2e915f465245272de0
Simon Liu <cyamonide@gmail.com>
2019-05-20 04:38:28 +0000

    Update webhook.rb

--- webhook.rb
+++ webhook.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
 
 post '/payload' do
-  `git status`
+    puts "Received payload."
+    puts "Pulling..."
+    `git pull`
+    puts "Changing dir..."
+    Dir.chdir "cyamonide-me"
+    puts "Building..."
+    `ng build --prod`
+    puts "Removing old files..."
+    `rm -rf /var/www/html/*`
+    puts "Copying new files..."
+    `cp -r dist/cyamonide-me/* /var/www/html`
+    puts "All done!"
 end
